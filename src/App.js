37edfd6c7e59f0ca4716db5443c81a430809a304
Khristian Brooks <kh.brooks02@gmail.com>
2019-03-18 11:57:23 -0400

    Add an OAuth Link

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://secure.meetup.com/oauth2/authorize
+                  ?client_id=tdg6drsop9c4s51h8qdvl5998q
+                  &response_type=token
+                  &redirect_uri=https://meetup-oauth-sample.herokuapp.com/"
+						target="_blank"
+					>
+						OAuth
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;

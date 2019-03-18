32a9173b8704887b1e29f4e38796e42c72ee5fcd
Khristian Brooks <kh.brooks02@gmail.com>
2019-03-18 12:08:45 -0400

    fix OAuth Link -- remove spaces

--- src/App.js
+++ src/App.js
@@ -13,11 +13,9 @@ class App extends Component {
 					</p>
 					<a
 						className="App-link"
-						href="https://secure.meetup.com/oauth2/authorize
-                  ?client_id=tdg6drsop9c4s51h8qdvl5998q
-                  &response_type=token
-                  &redirect_uri=https://meetup-oauth-sample.herokuapp.com/"
+						href="https://secure.meetup.com/oauth2/authorize?client_id=tdg6drsop9c4s51h8qdvl5998q&response_type=token&redirect_uri=https://meetup-oauth-sample.herokuapp.com/"
 						target="_blank"
+						rel="noopener noreferrer"
 					>
 						OAuth
 					</a>

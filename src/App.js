012f5a9c990251b2b02ff325ee593e4bd63cd2e0
Khristian Brooks <kh.brooks02@gmail.com>
2019-03-18 13:12:43 -0400

    make a request using OAuth

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import './App.css';
 
 class App extends Component {
 	render() {
-		const key = window.location.href.slice(56, 88);
-		console.log(key);
+		const token = window.location.href.slice(56, 88);
 		return (
 			<div className="App">
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://secure.meetup.com/oauth2/authorize?client_id=tdg6drsop9c4s51h8qdvl5998q&response_type=token&redirect_uri=https://meetup-oauth-sample.herokuapp.com/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						OAuth
-					</a>
+					{token.length ? (
+						fetch(
+							`https://api.meetup.com/pro/gdg/groups/?zip=11216&radius=10&access_token=${token}`
+						)
+							.then(function(response) {
+								return response.json();
+							})
+							.then(function(myJson) {
+								console.log(JSON.stringify(myJson));
+							})
+					) : (
+						<div>
+							<img src={logo} className="App-logo" alt="logo" />
+							<p>
+								Edit <code>src/App.js</code> and save to reload.
+							</p>
+							<a
+								className="App-link"
+								href="https://secure.meetup.com/oauth2/authorize?client_id=tdg6drsop9c4s51h8qdvl5998q&response_type=token&redirect_uri=https://meetup-oauth-sample.herokuapp.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								OAuth
+							</a>
+						</div>
+					)}
 				</header>
 			</div>
 		);
